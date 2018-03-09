485c24eb920475f15b90b799e1c763bfd0cfb251
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-09 04:47:07 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
-#write your code here
+require 'pry'
 
-def countdown
+def countdown(x)
+  while x > 0 
+    puts "#{x} second(s)!".upcase
+    sleep 1
+    x -= 1
+  end
+  return "happy new year!".upcase
+end
+
+def countdown_with_sleep(x)
+  sleep x
 end
